af1904670290b11308a37c0f76472de37e1ce260
Gleb <gleb@gleblebedev.com>
2022-12-07 23:29:17 +0000

    First version of a sample project

---
+++ Source/SampleProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using Urho3DNet;
+
+namespace SampleProject
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            using (var context = new Context())
+            {
+                using (var app = new SampleApplication(context))
+                {
+                    app.Run();
+                }
+            }
+        }
+    }
+}
---
+++ Source/SampleProject/SampleApplication.cs
@@ -0,0 +1,33 @@
+﻿using Urho3DNet;
+
+namespace SampleProject;
+
+class SampleApplication : Application
+{
+    private Scene _scene;
+
+    public SampleApplication(Context context) : base(context)
+    {
+    }
+
+    public override void Setup()
+    {
+        base.Setup();
+    }
+
+    public override void Start()
+    {
+        base.Start();
+
+        _scene = Context.CreateObject<Scene>();
+        _scene.AddRef();
+        _scene.LoadFile("Scenes/Scene.xml");
+        Context.Renderer.SetViewport(0, new Viewport(Context, _scene, _scene.GetComponent<Camera>(true)));
+    }
+
+    public override void Stop()
+    {
+        _scene.ReleaseRef();
+        base.Stop();
+    }
+}
